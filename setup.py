e65c0ca9490f5277ca68e7298652df64bf8e7dce
Luiza Pozzobon <luiza.pozzobon@gmail.com>
2022-06-17 09:05:51 -0300

    chore: add setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="score_sde",
+    packages=find_packages("scode_sde"),
+    version="0.1.0",
+    description="score_sde package from NVlabs repo",
+    author="NVlabs",
+    license="MIT",
+)
